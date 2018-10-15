5ae77f396bd3b428e878b671ad5cf36c21a342d3
vinitkr28 <vinitkr28@gmail.com>
2018-10-15 11:44:17 +0530

    Initial commit

---
+++ app/src/main/java/git1a/vinit/git1c/MainActivity.java
@@ -0,0 +1,18 @@
+package git1a.vinit.git1c;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void TestUpload(){
+        int i = 0;
+        i = i + 1;
+    }
+}
